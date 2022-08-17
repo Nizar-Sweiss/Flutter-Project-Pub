cdcf05233b420c17e3a8a2c94c73214d0ff427db
Nizar Sweiss <Nizarsweiss00@gmail.com>
2022-08-17 11:34:02 +0300

    Chaldean Widget Added   and network image
    i added the column so i can add more widget to the body , so in the body i added images using both way (locally and network ) also i added the text alignment and I edited the containers texts and back color

--- project2/lib/main.dart
+++ project2/lib/main.dart
@@ -1,115 +1,77 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(HomePage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        backgroundColor: Colors.black87,
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Image.network(
+            "https://user-images.githubusercontent.com/51419598/152648731-567997ec-ac1c-4a9c-a816-a1fb1882abbe.png",
+            width: 50,
+            height: 50,
+            alignment: Alignment.center,
+          ),
+        ),
+        drawer: Drawer(),
+        body: Column(
+          children: [
+            Container(
+              alignment: Alignment.topCenter,
+              color: Color.fromARGB(255, 173, 2, 2),
+              // padding: EdgeInsetsDirectional.all(20),
+              padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 42),
+              child: Text(
+                "Welcome to Project 2",
+                style: TextStyle(
+                    wordSpacing: 7.6,
+                    fontSize: 18,
+                    color: Color.fromARGB(153, 255, 255, 255),
+                    fontWeight: FontWeight.normal),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              alignment: Alignment.topLeft,
+              color: Colors.transparent,
+              //padding: EdgeInsetsDirectional.all(5),
+              padding: EdgeInsets.fromLTRB(40, 15, 0, 0),
+              child: Text(
+                " This is the Second Container ",
+                style: TextStyle(
+                    wordSpacing: 7.6,
+                    fontSize: 18,
+                    color: Colors.white60,
+                    fontWeight: FontWeight.bold),
+              ),
             ),
+            Image (
+              image:AssetImage('assets/images/redtree.jpg'), width:300,height: 300 ,
+            )
+            /* Image.network
+           Image.network(
+              "https://loveshayariimages.in/wp-content/uploads/2021/10/free-New-Red-Wallpaper-scaled.jpg",
+              alignment: Alignment.center,
+              width: 50,
+              height: 50,
+            ),
+            */
+
+            // shadowColor: Color.fromARGB(153, 255, 255, 255),
+            // elevation: 25,
+            // width: 200,
+            // height: 250,
           ],
+          //here was Container
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
