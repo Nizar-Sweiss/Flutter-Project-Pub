065100c3babfcdf4f71243395717d9f048d6809a
Nizar Sweiss <Nizarsweiss00@gmail.com>
2022-08-28 23:05:45 +0300

    enhancement
    th5 th5 edits

--- Labs/lab5/lib/main.dart
+++ Labs/lab5/lib/main.dart
@@ -21,6 +21,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           backgroundColor: mydefaultColor,
           appBar: AppBar(
@@ -30,72 +31,68 @@ class _HomeState extends State<Home> {
           body: ListView(
             children: [
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
+                    alignment: Alignment.topLeft,
                     margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child: Text("Option 1"),
+                    child: Text(
+                      "Option 1",
+                    ),
                   ),
-                  Row(
+                  Image.asset(
+                    myImage1,
+                    width: 240,
+                    height: 240,
+                  ),
+                  Column(
                     children: [
-                      Container(
-                        width: 240,
-                        height: 240,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage(myImage1),
-                          ),
+                      IconButton(
+                        onPressed: () {
+                          //action coe when button is pressed
+                        },
+                        icon: Icon(
+                          Icons.thumb_up_off_alt_outlined,
+                          color: Color.fromARGB(255, 0, 17, 255),
                         ),
                       ),
-                      Column(
-                        children: [
-                          IconButton(
-                            onPressed: () {
-                              //action coe when button is pressed
-                            },
-                            icon: Icon(
-                              Icons.thumb_up_off_alt_outlined,
-                              color: Color.fromARGB(255, 0, 17, 255),
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              //action coe when button is pressed
-                            },
-                            icon: Icon(
-                              Icons.check,
-                              color: Color.fromARGB(255, 2, 255, 10),
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              //action coe when button is pressed
-                            },
-                            icon: Icon(
-                              Icons.close_rounded,
-                              color: Color.fromARGB(255, 255, 4, 4),
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              //action coe when button is pressed
-                            },
-                            icon: Icon(
-                              Icons.star,
-                              color: Color.fromARGB(255, 255, 175, 2),
-                            ),
-                          ),
-                        ],
+                      IconButton(
+                        onPressed: () {
+                          // ToDo: action coe when button is pressed
+                        },
+                        icon: Icon(
+                          Icons.check,
+                          color: Color.fromARGB(255, 2, 255, 10),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          //action coe when button is pressed
+                        },
+                        icon: Icon(
+                          Icons.close_rounded,
+                          color: Color.fromARGB(255, 255, 4, 4),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          //action coe when button is pressed
+                        },
+                        icon: Icon(
+                          Icons.star,
+                          color: Color.fromARGB(255, 255, 175, 2),
+                        ),
                       ),
                     ],
                   ),
-                   Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
                   IconButton(
                     onPressed: () {
-                      //action coe when button is pressed
+                      //action when button is pressed
                       setState(() {
-                        myImage1 = "images/btv5.jpg";
+                        // myImage1 = "images/btv5.jpg";
+                        myImage1 == "images/btv1.jpg"
+                            ? myImage1 = "images/btv5.jpg"
+                            : myImage1 = "images/btv1.jpg";
                       });
                     },
                     icon: Icon(
@@ -106,10 +103,8 @@ class _HomeState extends State<Home> {
                   ),
                 ],
               ),
-                ],
-              ),
-              
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
                     margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
@@ -161,7 +156,6 @@ class _HomeState extends State<Home> {
                           ),
                         ],
                       ),
-                      
                       Container(
                         width: 240,
                         height: 240,
@@ -171,30 +165,28 @@ class _HomeState extends State<Home> {
                           ),
                         ),
                       ),
-                       Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      //action coe when button is pressed
-                      setState(() {
-                        myImage2 = "images/btv6.jpg";
-                      });
-                    },
-                    icon: Icon(
-                      Icons.send,
-                      color: Color.fromARGB(255, 0, 183, 255),
-                      size: 40,
-                    ),
-                  ),
-                ],
-              ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          IconButton(
+                            onPressed: () {
+                              //action coe when button is pressed
+                              setState(() {
+                                myImage2 = "images/btv6.jpg";
+                              });
+                            },
+                            icon: Icon(
+                              Icons.send,
+                              color: Color.fromARGB(255, 0, 183, 255),
+                              size: 40,
+                            ),
+                          ),
+                        ],
+                      ),
                     ],
                   )
-                  
                 ],
               ),
-                
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -323,91 +315,93 @@ class _HomeState extends State<Home> {
                 child: Container(
                   padding: EdgeInsets.all(20), //padding of outer Container
                   child: DottedBorder(
-                    color: Color.fromARGB(
-                        0, 0, 208, 255), //color of dotted/dash line
+                    color: Colors.black26, //color of dotted/dash line
                     strokeWidth: 3, //thickness of dash/dots
                     dashPattern: [10, 6],
 
                     //dash patterns, 10 is dash width, 6 is space width
-                    child: Center(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Card(
-                            margin: EdgeInsets.fromLTRB(20, 0, 10, 0),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(80),
-                            ),
-                            child: IconButton(
-                              onPressed: () {
-                                //action coe when button is pressed
-                                //showToastMessage("You rated us So Bad Rating ! ");
-                                setState(() {
-                                  mydefaultColor =
-                                      Color.fromARGB(255, 236, 6, 6);
-                                });
-                              },
-                              icon: Icon(
-                                Icons.circle,
-                                color: Colors.red,
-                              ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          color: Colors.red,
+                          width: 20,
+                          height: 20,
+                        ),
+                        Card(
+                          margin: EdgeInsets.fromLTRB(20, 0, 10, 0),
+                          color: Colors.green,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(80),
+                          ),
+                          child: IconButton(
+                            onPressed: () {
+                              //action coe when button is pressed
+                              //showToastMessage("You rated us So Bad Rating ! ");
+                              setState(() {
+                                mydefaultColor = Color.fromARGB(255, 236, 6, 6);
+                              });
+                            },
+                            icon: Icon(
+                              Icons.circle,
+                              color: Colors.red,
                             ),
                           ),
-                          Card(
-                            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(80),
-                            ),
-                            child: IconButton(
-                              onPressed: () {
-                                //action coe when button is pressed
+                        ),
+                        Card(
+                          margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(80),
+                          ),
+                          child: IconButton(
+                            onPressed: () {
+                              //action coe when button is pressed
 
-                                //showToastMessage("Show Toast Message on Flutter");
-                                setState(() {
-                                  mydefaultColor = Colors.green;
-                                });
-                              },
-                              icon: Icon(
-                                Icons.circle,
-                                color: Color.fromARGB(255, 0, 255, 34),
-                              ),
+                              //showToastMessage("Show Toast Message on Flutter");
+                              setState(() {
+                                mydefaultColor = Colors.green;
+                              });
+                            },
+                            icon: Icon(
+                              Icons.circle,
+                              color: Color.fromARGB(255, 0, 255, 34),
                             ),
                           ),
-                          Card(
-                            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(80),
-                            ),
-                            child: IconButton(
-                              onPressed: () {
-                                //action coe when button is pressed
-                                setState(() {
-                                  mydefaultColor = Colors.blue;
-                                });
-                              },
-                              icon: Icon(
-                                Icons.circle,
-                                color: Color.fromARGB(255, 0, 30, 255),
-                              ),
+                        ),
+                        Card(
+                          margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(80),
+                          ),
+                          child: IconButton(
+                            onPressed: () {
+                              //action coe when button is pressed
+                              setState(() {
+                                mydefaultColor = Colors.blue;
+                              });
+                            },
+                            icon: Icon(
+                              Icons.circle,
+                              color: Color.fromARGB(255, 0, 30, 255),
                             ),
                           ),
-                          Card(
-                            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(100),
-                            ),
-                            child: IconButton(
-                              onPressed: () {
-                                //action coe when button is pressed
-                                setState(() {
-                                  mydefaultColor = Colors.black;
-                                });
-                              },
-                              icon: Icon(Icons.circle, color: Colors.black),
-                            ),
+                        ),
+                        Card(
+                          margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100),
                           ),
-                        ],
-                      ),
+                          child: IconButton(
+                            onPressed: () {
+                              //action coe when button is pressed
+                              setState(() {
+                                mydefaultColor = Colors.black;
+                              });
+                            },
+                            icon: Icon(Icons.circle, color: Colors.black),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
